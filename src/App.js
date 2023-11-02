4b2687b2c1be3edf529072a2ffbcb4e424e7aefc
wizleriq <103083336+wizleriq@users.noreply.github.com>
2023-11-02 13:15:56 +0100

    init general update from all components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Write from './Write';
+import Navbar from './components/Navbar';
+import Settings from './components/Settings';
+// import Header from './components/Header';
+import Home from './components/Home';
+import Single from './components/Single';
+import Login from './components/Login';
+import Register from './Register';
+import { Routes, Route } from "react-router-dom";import Details from './components/Details';
+
+import SinglePost from './components/SinglePost';
+
 
 function App() {
+  const user = true; 
+
+  const renderElement = () => {
+    return user ? <Home /> : <Register />;
+  };
+
+  const renderLogin = () => {
+    return user ? <Home /> : <Login />;
+  };
+
+  const renderAbout = () => {
+    return user ? <Home /> : null;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={renderElement()} />
+        <Route path="/" element={renderLogin()} />
+        <Route path="/write" element={ user ? <Write /> : <Register /> } />
+        <Route path="/about" element={renderAbout} />
+        <Route path='/singlepost' element={<SinglePost />} /> 
+        <Route path="/login" element={<Login /> } />
+        <Route path="/register" element={<Register /> } />
+        <Route path="/settings" element={ user ? <Settings /> : <Register /> } />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+    </>
   );
 }
 
 export default App;
+
+
